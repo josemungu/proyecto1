02c43fc13222c8c895da2d4f769bd259b195516b
josemungu <jose03mg@gmail.com>
2022-03-30 09:56:01 +0200

    Añado un syso mas

--- Jose/src/HolaMundo.java
+++ Jose/src/HolaMundo.java
@@ -1,9 +1,10 @@
-
 public class HolaMundo {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
 		System.out.println("Añado una linea");
+		System.out.println("Añado cambio 1");
+
 
 	}
 
